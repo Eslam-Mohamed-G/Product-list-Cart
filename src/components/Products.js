6f6ac56ddd70d8d787982eee76b8350ed2539543
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2024-11-24 23:53:15 +0200

    add design

--- src/components/Products.js
+++ src/components/Products.js
@@ -6,7 +6,6 @@ import increment from '../images/icon-increment-quantity.svg'
 
 const Products = () => {
   const [data, setData] = useState([]);
-
   useEffect(() => {
     fetch("/data.json")
       .then((response) => {
@@ -20,6 +19,8 @@ const Products = () => {
       })
       .catch((error) => console.error("Error fetching data:", error));
   }, []);
+
+  const quantity = 0;
   return (
     <div className="w-full box-border">
       <h1 className="font-bold text-[28px] mb-5">Desserts</h1>
@@ -28,25 +29,27 @@ const Products = () => {
           <div key={index} className="w-full mb-1 md:w-1/2 xl:w-1/3">
             <div className="w-full pr-5 pb-3">
               <div className=" relative mb-7">
-                <div className="rounded-[10px] overflow-hidden">
+                <div className={`rounded-[10px] overflow-hidden border border-[${quantity == 0 ? "var(--Rose-50)" : "var(--Red)"}]`}>
                   <img
                     className="w-full block"
                     src={item.image.desktop}
                     alt={item.name}
                   />
                 </div>
+                
                 {/* add to cart start */}
-                <div className="w-40 flex items-center justify-center group bg-[var(--Rose-50)] border rounded-[20px] border-[var(--Rose-900)] px-4 py-2 cursor-pointer hover:bg-[var(--Red)] absolute bottom-0 left-1/2 transform translate-x-[-50%] translate-y-[50%]">
-                  <div className="flex gap-1 w-fit group-hover:hidden">
-                    <img src={iconCart} /> Add to Cart
-                  </div>
-
-                  {/* buttons */}
-                  <div className="hidden group-hover:flex items-center justify-center w-full text-center relative">
-                    <span className="w-4 h-4 rounded-[50%] flex items-center justify-center border border-[var(--Rose-50)] absolute left-0"><img src={decrement} alt="decrement" /></span>
-                    <p className="text-[var(--Rose-50)]">0</p>
-                    <span className="w-4 h-4 rounded-[50%] flex items-center justify-center border border-[var(--Rose-50)] absolute right-0"><img src={increment} alt="increment" /></span>
-                  </div>
+                <div className={`w-40 h-10 px-4 flex items-center justify-center ${quantity == 0 ? "bg-[var(--Rose-50)]" : "bg-[var(--Red)]"} border rounded-[20px] border-[${quantity === 0 ? "var(--Rose-900)" : "var(--Red)"}] hover:border-[var(--Red)] cursor-pointer hover:text-[var(--Red)] transition-all absolute bottom-0 left-1/2 transform translate-x-[-50%] translate-y-[50%]`}>
+                  {quantity === 0
+                    ? 
+                      <div className="flex gap-1 w-fit">
+                        <img src={iconCart} /> Add to Cart
+                      </div>
+                    : 
+                    <div div className="flex items-center justify-center w-full text-center relative">
+                      <span className="w-4 h-4 rounded-[50%] flex items-center justify-center border border-[var(--Rose-50)] absolute left-0"><img src={decrement} alt="decrement" /></span>
+                      <p className="text-[var(--Rose-50)]">0</p>
+                      <span className="w-4 h-4 rounded-[50%] flex items-center justify-center border border-[var(--Rose-50)] absolute right-0"><img src={increment} alt="increment" /></span>
+                    </div> }
                 </div>
                 {/* add to cart end */}
               </div>
